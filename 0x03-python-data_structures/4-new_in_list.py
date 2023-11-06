c3ffa1e39383825a2aa06ec4cf16abcabc8f34e3
Abdellatif <abdelt.2012@gmail.com>
2023-11-06 02:13:54 +0100

    task done.

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,5 +4,5 @@ def new_in_list(my_list, idx, element):
         return my_list
     else:
         cp_list = my_list.copy()
-        cp_list(idx) = element
+        cp_list[idx] = element
         return cp_list
