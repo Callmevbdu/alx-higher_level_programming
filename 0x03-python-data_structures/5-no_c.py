6e8e0089b7c6d3d669ad6cde96fa659c4c76df01
Abdellatif <abdelt.2012@gmail.com>
2023-11-06 02:26:40 +0100

    task done.

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if not my_string:
+        return None
+    else:
+        for i in my_string:
+            my_list.append(i)
+        my_list.remove("c")
+        my_list.remove("C")
+
