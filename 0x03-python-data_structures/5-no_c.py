4f396cfe369ccbbce99093ddfef9f56b43f91330
Abdellatif <abdelt.2012@gmail.com>
2023-11-06 02:30:18 +0100

    task done.

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,8 +3,9 @@ def no_c(my_string):
     if not my_string:
         return None
     else:
-        for i in my_string:
-            my_list.append(i)
-        my_list.remove("c")
-        my_list.remove("C")
+        croped = ""
+        for i in range(len(my_string)):
+            if (my_string[i] != 'c' and my_string[i] != 'C'):
+                croped += my_string[i]
+        return croped
 
