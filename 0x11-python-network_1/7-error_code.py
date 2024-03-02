bba0f270a136e1aa962e3066759a01b0b5bd4de1
Abdellatif <abdelt.2012@gmail.com>
2024-03-02 02:55:25 +0100

    Done.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request to the URL and displays
+the body of the response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
+    except requests.exceptions.RequestException as e:
+        print("Error code:", e.response.status_code)
