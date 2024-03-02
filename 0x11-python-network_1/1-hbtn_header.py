21e8133164f73da442dcdd83a4312d404447068d
Abdellatif <abdelt.2012@gmail.com>
2024-03-02 02:30:44 +0100

    Done.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import urllib.parse
 import urllib.request
 import sys
 
@@ -12,11 +11,9 @@ passed URL with the email as a parameter, and displays the body of the response
 """
 
 if __name__ == "__main__":
-    url, email = sys.argv[1:]
+    url = sys.argv[1:]
 
-    data = urllib.parse.urlencode({'email': email})
-    data = data.encode('utf-8')
+    req = urllib.request.Request(url)
 
-    with urllib.request.urlopen(url, data=data) as response:
-        body = response.read().decode('utf-8')
-        print(body)
+    with urllib.request.urlopen(req) as response:
+        print(dict(response.headers).get("X-Request-Id"))
