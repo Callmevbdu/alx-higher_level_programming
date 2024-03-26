20e882123207d6a4dab800c3db91ef431f6ec1d6
Abdellatif <abdelt.2012@gmail.com>
2024-03-26 17:26:39 +0000

    Done.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,10 +5,11 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-});
+fs.readFile(process.argv[2], 'utf-8',
+	function (err, data) {
+		if (err) {
+			console.error(err);
+		return;
+		}
+		console.log(data);
+	});
