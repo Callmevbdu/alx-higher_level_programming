479b94387f63c6631e3424d09b7ec75edb238988
Abdellatif <abdelt.2012@gmail.com>
2024-03-26 20:21:17 +0000

    Done.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// a script that gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`File ${filePath} written successfully`);
+  });
+});
