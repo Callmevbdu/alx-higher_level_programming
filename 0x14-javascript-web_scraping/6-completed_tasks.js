79704d86fcdc6ab4113833bd54245ba258e4a59c
Abdellatif <abdelt.2012@gmail.com>
2024-03-26 20:36:47 +0000

    Done.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,20 +4,20 @@
 const request = require('request');
 const completedTasks = {};
 
-request.get(process.argv[2], (error, response, body) => {
+request.get(process.argv[2], { json: true }, (error, response, body) => {
   if (error) {
     console.log(error);
     return;
   }
 
-  const tasks = JSON.parse(body);
-
-  tasks.forEach((task) => {
+  body.forEach((task) => {
     if (task.completed) {
-      if (completedTasks[task.userId]) {
-        completedTasks[task.userId]++;
-      } else {
+      if (!completedTasks[task.userId]) {
         completedTasks[task.userId] = 1;
+      } else {
+        completedTasks[task.userId] += 1;
       }
     }
   });
+  console.log(completedTasks);
+});
