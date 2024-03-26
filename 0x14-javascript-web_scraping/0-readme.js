2ab217ee779c210c7519bbc21c5303371353b81d
Abdellatif <abdelt.2012@gmail.com>
2024-03-26 17:51:43 +0000

    Done.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-
+// a script that reads and prints the content of a file.
 const fs = require('fs');
 const filePath = process.argv[2];
 fs.readFile(filePath, 'utf-8', (err, data) => {
