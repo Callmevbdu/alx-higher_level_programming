c840ea8ee90a8ae4ee5b77c27481ddd0034bfb34
Abdellatif <abdelt.2012@gmail.com>
2024-04-04 02:46:18 +0000

    Done.

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,2 @@
 // Select the header element using document.querySelector
-const header = document.querySelector('header');
-
-// Set the text color of the header to red (#FF0000)
-header.style.color = '#FF0000';
+document.querySelector('header').style.color = '#FF0000';
